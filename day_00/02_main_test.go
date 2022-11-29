d82ab9e42341d7f2d09e8a32069879778c7aa48d
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2022-11-29 17:43:41 +0000

    Renaming test files and adding another
    To prove that I can have multiple tests for the same function,
    I'm adding another test file.

--- day_00/02_main_test.go
+++ day_00/02_main_test.go
@@ -2,9 +2,9 @@ package day_00
 
 import "testing"
 
-func Test_sayHello(t *testing.T) {
-	name := "Bob"
-	want := "Hello Bob"
+func Test_sayHello_02(t *testing.T) {
+	name := "Beth"
+	want := "Hello Beth"
 
 	if got := sayHello(name); got != want {
 		t.Errorf("hello() = %q, want %q", got, want)
