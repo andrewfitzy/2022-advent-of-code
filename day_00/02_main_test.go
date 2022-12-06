2584e1a90ee5ef108b9c9fa2716e6db7c0a7f0c3
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2022-12-06 14:02:23 +0000

    Updating prototype folder

--- day_00/02_main_test.go
+++ day_00/02_main_test.go
@@ -1,9 +1,10 @@
 package day_00
 
 import "testing"
+import "github.com/andrewfitzy/2022-advent-of-code/util"
 
 func Test_sayHello_02(t *testing.T) {
-	name := "Beth"
+	name := util.GetFileContent("main_input_01.txt")
 	want := "Hello Beth"
 
 	if got := sayHello(name); got != want {
