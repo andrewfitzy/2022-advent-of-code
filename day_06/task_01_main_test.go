81575a5937b737d40c54be00cfad66ba7e1d4420
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2022-12-06 06:41:43 +0000

    Adding day 6 task 2

--- day_06/task_01_main_test.go
+++ day_06/task_01_main_test.go
@@ -4,6 +4,19 @@ import "testing"
 
 func Test_solve01_with_input_01(t *testing.T) {
 
+	input := "mjqjpqmgbljsphdztnvjfqwrcgsmlb"
+
+	want := 7
+
+	got := solve01(input)
+
+	if want != got {
+		t.Errorf("solve01() = %v, want %v", got, want)
+	}
+}
+
+func Test_solve01_with_input_02(t *testing.T) {
+
 	input := "bvwbjplbgvbhsrlpgdmjqwftvncz"
 
 	want := 5
@@ -15,7 +28,7 @@ func Test_solve01_with_input_01(t *testing.T) {
 	}
 }
 
-func Test_solve01_with_input_02(t *testing.T) {
+func Test_solve01_with_input_03(t *testing.T) {
 	input := "nppdvjthqldpwncqszvftbrmjlhg"
 
 	want := 6
@@ -27,7 +40,7 @@ func Test_solve01_with_input_02(t *testing.T) {
 	}
 }
 
-func Test_solve01_with_input_03(t *testing.T) {
+func Test_solve01_with_input_04(t *testing.T) {
 	input := "nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"
 
 	want := 10
@@ -39,7 +52,7 @@ func Test_solve01_with_input_03(t *testing.T) {
 	}
 }
 
-func Test_solve01_with_input_04(t *testing.T) {
+func Test_solve01_with_input_05(t *testing.T) {
 	input := "zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"
 
 	want := 11
@@ -51,7 +64,7 @@ func Test_solve01_with_input_04(t *testing.T) {
 	}
 }
 
-func Test_solve01_with_input_05(t *testing.T) {
+func Test_solve01_with_input_06(t *testing.T) {
 	input := get_file_content("task_01_input_01.txt")
 
 	want := 1804
