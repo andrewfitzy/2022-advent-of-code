6c1d22861ca4fcd82167c5c81547744c9c3c55a2
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2022-12-07 13:44:23 +0000

    Adding day 7 task 2

---
+++ day_07/task_02_main_test.go
@@ -0,0 +1,28 @@
+package day_07
+
+import "testing"
+import "github.com/andrewfitzy/2022-advent-of-code/util"
+
+func Test_solve02_with_input_01(t *testing.T) {
+	input := util.GetFileContent("task_01_input_01.txt")
+
+	want := 24933642
+
+	got := solve02(input)
+
+	if want != got {
+		t.Errorf("solve02() = %v, want %v", got, want)
+	}
+}
+
+func Test_solve02_with_input_02(t *testing.T) {
+	input := util.GetFileContent("task_01_input_02.txt")
+
+	want := 942298
+
+	got := solve02(input)
+
+	if want != got {
+		t.Errorf("solve02() = %v, want %v", got, want)
+	}
+}
