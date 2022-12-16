fd1464aae05dfdc4c9c677b08967c5b4459644e9
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2022-12-16 19:37:03 +0000

    Initial commit of day 12 task 01 - no real functionality though

---
+++ day_12/task_01_main_test.go
@@ -0,0 +1,28 @@
+package day_12
+
+import "testing"
+import "github.com/andrewfitzy/2022-advent-of-code/util"
+
+func Test_solve01_with_input_01(t *testing.T) {
+	input := util.GetFileContent("task_01_input_01.txt")
+
+	want := 31
+
+	got := solve01(input)
+
+	if want != got {
+		t.Errorf("solve01() = %v, want %v", got, want)
+	}
+}
+
+// func Test_solve01_with_input_02(t *testing.T) {
+// 	input := util.GetFileContent("task_01_input_02.txt")
+
+// 	want := 0
+
+// 	got := solve01(input)
+
+// 	if want != got {
+// 		t.Errorf("solve01() = %v, want %v", got, want)
+// 	}
+// }
