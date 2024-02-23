490fb6fdfcd193e1c45856371b4112e738d5fc89
Andrew Fitzpatrick <andrewpfitzpatrick@gmail.com>
2024-02-23 19:00:17 +0000

    Deleting the actual puzzle inputs

--- day_xx/task_01_main_test.go
+++ day_xx/task_01_main_test.go
@@ -2,8 +2,6 @@ package day_xx
 
 import (
 	"testing"
-
-	"github.com/andrewfitzy/2022-advent-of-code/util"
 )
 
 func Test_solve01_with_demo_data_01(t *testing.T) {
@@ -18,14 +16,14 @@ func Test_solve01_with_demo_data_01(t *testing.T) {
 	}
 }
 
-func Test_solve01_with_real_data_01(t *testing.T) {
-	input := util.GetFileContent("puzzle_input.txt")
+// func Test_solve01_with_real_data_01(t *testing.T) {
+// 	input := util.GetFileContent("puzzle_input.txt")
 
-	want := "Hello World"
+// 	want := "Hello World"
 
-	got := solve01(input)
+// 	got := solve01(input)
 
-	if want != got {
-		t.Errorf("solve01() = %v, want %v", got, want)
-	}
-}
+// 	if want != got {
+// 		t.Errorf("solve01() = %v, want %v", got, want)
+// 	}
+// }
--- day_xx/task_02_main_test.go
+++ day_xx/task_02_main_test.go
@@ -2,8 +2,6 @@ package day_xx
 
 import (
 	"testing"
-
-	"github.com/andrewfitzy/2022-advent-of-code/util"
 )
 
 func Test_solve02_with_demo_data_01(t *testing.T) {
@@ -18,14 +16,14 @@ func Test_solve02_with_demo_data_01(t *testing.T) {
 	}
 }
 
-func Test_solve02_with_real_data_01(t *testing.T) {
-	input := util.GetFileContent("puzzle_input.txt")
+// func Test_solve02_with_real_data_01(t *testing.T) {
+// 	input := util.GetFileContent("puzzle_input.txt")
 
-	want := "Hello World"
+// 	want := "Hello World"
 
-	got := solve02(input)
+// 	got := solve02(input)
 
-	if want != got {
-		t.Errorf("solve02() = %v, want %v", got, want)
-	}
-}
+// 	if want != got {
+// 		t.Errorf("solve02() = %v, want %v", got, want)
+// 	}
+// }
